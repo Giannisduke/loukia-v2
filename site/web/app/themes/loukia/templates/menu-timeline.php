fa7af812e444508a2a007189e6641a875a16b109
Giannisduke <admin@eboy.gr>
2021-01-23 17:11:08 +0200

    timeline dates

--- site/web/app/themes/loukia/templates/menu-timeline.php
+++ site/web/app/themes/loukia/templates/menu-timeline.php
@@ -8,16 +8,16 @@
    'post_status' => 'publish',
    'posts_per_page' => -1,
    'orderby' => 'date',
-   'order' => 'ASC',
+   'order' => 'DESC',
 );
 
 $loop = new WP_Query( $args ); $counter_menu = -1;
-$post_date = get_the_date( 'n/y' );
+//$post_date = the_time('F Y');
 ?>
 <nav class="navbar navbar-expand-lg navbar-light navbar-right first">
 <div id="list-example" class="list-group">
 <?php while ( $loop->have_posts() ) : $loop->the_post(); $counter_menu++; ?>
-   <a class="list-group-item list-group-item-action" href="#section__<?php echo $counter_menu; ?>"><?php echo $post_date; ?></a>
+   <a class="list-group-item list-group-item-action" href="#section__<?php echo $counter_menu; ?>"><?php the_time('F Y'); ?></a>
 <?php endwhile; ?>
 </div>
 </nav>

847b2b21455ccdca26bf2761761eb3ce002ee8df
Giannisduke <admin@eboy.gr>
2021-01-27 08:21:25 +0200

    blog

--- site/web/app/themes/loukia/templates/content.php
+++ site/web/app/themes/loukia/templates/content.php
@@ -1,9 +1,13 @@
-<article <?php post_class(); ?>>
+<article <?php post_class(''); ?>>
   <header>
-    <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     <?php get_template_part('templates/entry-meta'); ?>
+    <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <div class="entry-summary">
+      <?php the_excerpt(); ?>
+    </div>
   </header>
-  <div class="entry-summary">
-    <?php the_excerpt(); ?>
-  </div>
+  <featured>
+    image
+  </featured>
+
 </article>

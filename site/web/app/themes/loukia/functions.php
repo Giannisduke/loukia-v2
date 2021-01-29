389e9b7806bc971aad55d3648b17bbe5fbc0c4fd
Giannisduke <admin@eboy.gr>
2021-01-29 23:27:29 +0200

    blog

--- site/web/app/themes/loukia/functions.php
+++ site/web/app/themes/loukia/functions.php
@@ -253,3 +253,18 @@ function get_product_description() {
   the_content();
 }
 add_action ('woocommerce_single_product_summary', 'get_product_description', 35 );
+
+add_filter( 'get_the_archive_title', function ($title) {
+    if ( is_category() ) {
+            $title = single_cat_title( '', false );
+        } elseif ( is_tag() ) {
+            $title = single_tag_title( '', false );
+        } elseif ( is_author() ) {
+            $title = '<span class="vcard">' . get_the_author() . '</span>' ;
+        } elseif ( is_tax() ) { //for custom post types
+            $title = sprintf( __( '%1$s' ), single_term_title( '', false ) );
+        } elseif (is_post_type_archive()) {
+            $title = post_type_archive_title( '', false );
+        }
+    return $title;
+});
--- site/web/app/themes/loukia/templates/content-single.php
+++ site/web/app/themes/loukia/templates/content-single.php
@@ -5,11 +5,22 @@
       <?php get_template_part('templates/entry-meta'); ?>
     </header>
     <div class="entry-content">
-      <?php the_content(); ?>
+      <div class="col-24 p-0">
+      <?php
+      if ( get_the_content() ) {
+      the_content();
+      echo '<hr>';
+      }
+      ?>
+      </div>
+      <div class="col-24 p-0">
+
+      <?php get_template_part('templates/portfolio-galery'); ?>
+    </div>
     </div>
     <footer>
       <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'loukia'), 'after' => '</p></nav>']); ?>
     </footer>
-    <?php comments_template('/templates/comments.php'); ?>
+    <?php //comments_template('/templates/comments.php'); ?>
   </article>
 <?php endwhile; ?>

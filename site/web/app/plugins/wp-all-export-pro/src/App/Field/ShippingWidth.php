b069f83a462561c30da833a1f3842dc6c701de80
Giannisduke <admin@eboy.gr>
2021-01-30 21:14:13 +0200

    wp-all-export-pro

---
+++ site/web/app/plugins/wp-all-export-pro/src/App/Field/ShippingWidth.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Wpae\App\Field;
+
+
+class ShippingWidth extends Field
+{
+    const SECTION = 'shipping';
+
+    public function getValue($snippetData)
+    {
+        $shippingData = $this->feed->getSectionFeedData(self::SECTION);
+
+        if($shippingData['dimensions'] == 'useWooCommerceProductValues') {
+
+            $currentUnit = get_option('woocommerce_dimension_unit');
+            $toUnit = $shippingData['convertTo'];
+
+            $product = $_product = wc_get_product($this->entry->ID);
+            if($currentUnit !== $toUnit) {
+                $width = wc_get_dimension($product->width, $currentUnit, $toUnit);
+            } else {
+                $width = $product->width;
+            }
+
+            return $width . ' '.$toUnit;
+        } else {
+            return $this->replaceSnippetsInValue($shippingData['dimensionsCV'], $snippetData);
+        }
+    }
+
+    public function getFieldName()
+    {
+        return 'shipping_width';
+    }
+}
---
+++ site/web/app/plugins/wp-all-export-pro/src/Csv/CsvWriter.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Wpae\Csv;
+
+
+class CsvWriter
+{
+    const CSV_STRATEGY_DEFAULT = 'default';
+    const CSV_STRATEGY_CUSTOM = 'custom';
+
+    private $csvStrategy;
+
+    public function __construct($csvStrategy = self::CSV_STRATEGY_DEFAULT)
+    {
+        $this->csvStrategy = $csvStrategy;
+    }
+
+    public function writeCsv($resource, $value, $delimiter)
+    {
+        foreach($value as $key => &$val ) {
+            if(is_object($val)) {
+                $val = '';
+            }
+        }
+
+        if($this->csvStrategy == self::CSV_STRATEGY_DEFAULT) {
+            fputcsv($resource, $value, $delimiter);
+        } else {
+            CsvRcfWriter::fputcsv($resource, $value, $delimiter);
+        }
+    }
+}

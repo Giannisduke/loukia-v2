6b82e3244585ea78d0a20d0b6bdbe17d2e20a8fe
Giannisduke <admin@eboy.gr>
2021-03-07 19:15:09 +0200

    Google verification

--- site/web/app/themes/loukia/templates/head.php
+++ site/web/app/themes/loukia/templates/head.php
@@ -2,5 +2,6 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="google-site-verification" content="26q6aw2xg_L3GPxladqY17f1F9l0QC0nSW4HDbjaw1g" />
   <?php wp_head(); ?>
 </head>

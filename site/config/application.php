2dad0514ea525cd5dcac0513a56219f7f04ef803
Giannisduke <admin@eboy.gr>
2021-08-05 17:23:23 +0300

    Header white

--- site/config/application.php
+++ site/config/application.php
@@ -91,7 +91,7 @@ Config::define('NONCE_SALT', env('NONCE_SALT'));
 /**
  * Custom Settings
  */
-Config::define('AUTOMATIC_UPDATER_DISABLED', true);
+Config::define('AUTOMATIC_UPDATER_DISABLED', false);
 Config::define('DISABLE_WP_CRON', env('DISABLE_WP_CRON') ?: false);
 // Disable the plugin and theme file editor in the admin
 Config::define('DISALLOW_FILE_EDIT', true);
